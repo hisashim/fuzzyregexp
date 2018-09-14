c100a3e12e0e17de4c45b868c973b5da204197b5
Hisashi Morita <hisashim@workbook.org>
2018-09-14 20:54:36 +0900

    Add a few user command examples with varied sensitibities

--- examples/moccur-typos.el
+++ examples/moccur-typos.el
@@ -13,7 +13,32 @@
 (require 'color-moccur)
 (require 'fuzzy-regexp)
 
-(defun moccur-typos (str)
-  "Show possible typographical errors of STR by single character"
+(defun moccur-typos-optimistic (str)
+  "Show possible typographical errors of STR, off by a single character.
+Show fewer candidates, avoiding false positives.
+  - Case sensitive
+  - Match whole words only"
   (interactive "MString: ")
-  (moccur (fuzzy-regexp str) t))
+  (let ((case-fold-search nil))
+    (moccur (fuzzy-regexp str t nil) t)))
+
+(defun moccur-typos-moderate (str)
+  "Show possible typographical errors of STR, off by a single character.
+Show moderate number of candidates.
+  - Case insensitive
+  - Match substrings"
+  (interactive "MString: ")
+  (let ((case-fold-search t))
+    (moccur (fuzzy-regexp str nil nil) t)))
+
+(defun moccur-typos-pessimistic (str)
+  "Show possible typographical errors of STR, off by a single character.
+Show many candidates, allowing false positives.
+  - Case insensitive
+  - Match substrings
+  - Include the search string itself"
+  (interactive "MString: ")
+  (let ((case-fold-search t))
+    (moccur (fuzzy-regexp str nil t) t)))
+
+(defalias 'moccur-typos 'moccur-typos-moderate)
