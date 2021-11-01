0641986f0484540d1642396b887d5359501ba3be
Hisashi Morita <hisashim@workbook.org>
2021-11-01 10:01:24 +0900

    Change prefix for internal functions, adopting popular naming convention

--- fuzzy-regexp.el
+++ fuzzy-regexp.el
@@ -18,7 +18,7 @@
 
 ;; string utilities
 
-(defun %fre-replace-substring (str rep start end)
+(defun fuzzy-regexp--replace-substring (str rep start end)
   "Replace substring of STR (from START (inclusive) to END (exclusive))
  with REP"
   (concat (substring str 0 start)
@@ -27,21 +27,21 @@
                 (t (error "replacement must be a string or a function")))
           (substring str end)))
 
-(defun %fre-remove-substring (str start end)
-  (%fre-replace-substring str "" start end))
+(defun fuzzy-regexp--remove-substring (str start end)
+  (fuzzy-regexp--replace-substring str "" start end))
 
-(defun %fre-insert-substring (str idx ins)
+(defun fuzzy-regexp--insert-substring (str idx ins)
   (concat (substring str 0 idx)
           ins
           (substring str idx)))
 
-(defun %fre-transpose-substring (str idx)
+(defun fuzzy-regexp--transpose-substring (str idx)
   (concat (substring str 0 idx)
           (substring str (+ idx 1) (+ idx 2))
           (substring str idx (+ idx 1))
           (substring str (+ idx 2))))
 
-(defun %fre-remove-if-match (str regexps)
+(defun fuzzy-regexp--remove-if-match (str regexps)
   (reduce (lambda (l e)
             (if (string-match e str)
                 l
@@ -51,7 +51,7 @@
 
 ;; varied patterns
 
-(defun %fre-patterns-one-char-removed (str)
+(defun fuzzy-regexp--patterns-one-char-removed (str)
   (cond ((<= (length str) 1) `(""))
         (t (let* ((body-indices (if (<= (length str) 2)
                                     '()
@@ -61,7 +61,8 @@
                                "]"
                                (substring str 1 nil)))
                   (body (map 'list
-                             (lambda (i) (%fre-remove-substring str i (+ i 1)))
+                             (lambda (i)
+                               (fuzzy-regexp--remove-substring str i (+ i 1)))
                              body-indices))
                   (lst (concat (regexp-quote (substring str 0 -1))
                                "[^"
@@ -69,44 +70,46 @@
                                "]")))
              `(,fst ,@body ,lst)))))
 
-(defun %fre-patterns-one-char-inserted (str ins)
+(defun fuzzy-regexp--patterns-one-char-inserted (str ins)
   (cond ((< (length str) 2) `(""))
         (t `(,@(map 'list
-                    (lambda (idx) (%fre-insert-substring str idx ins))
+                    (lambda (idx) (fuzzy-regexp--insert-substring str idx ins))
                     (number-sequence 1 (- (length str) 2)))
-             ,(%fre-insert-substring str -1 ins)))))
+             ,(fuzzy-regexp--insert-substring str -1 ins)))))
 
-(defun %fre-patterns-one-char-replaced (str rep)
+(defun fuzzy-regexp--patterns-one-char-replaced (str rep)
   (cond ((= (length str) 0) `(""))
         (t (map 'list
-                (lambda (i) (%fre-replace-substring str rep i (+ i 1)))
+                (lambda (i) (fuzzy-regexp--replace-substring str rep i (+ i 1)))
                 (number-sequence 0 (- (length str) 1))))))
 
-(defun %fre-patterns-one-char-transposed (str)
+(defun fuzzy-regexp--patterns-one-char-transposed (str)
   (cond ((<= (length str) 1) `(,str))
-        ((= (length str) 2) `(,(%fre-transpose-substring str 0)))
+        ((= (length str) 2) `(,(fuzzy-regexp--transpose-substring str 0)))
         (t (map 'list
-                (lambda (idx) (%fre-transpose-substring str idx))
+                (lambda (idx) (fuzzy-regexp--transpose-substring str idx))
                 (number-sequence 0 (- (length str) 2))))))
 
-(defun %fre-patterns-one-char-modified (str)
+(defun fuzzy-regexp--patterns-one-char-modified (str)
   (let* ((patterns
           (append
-           (%fre-remove-if-match str (%fre-patterns-one-char-removed str))
-           (%fre-patterns-one-char-inserted str ".")
-           (%fre-patterns-one-char-replaced
+           (fuzzy-regexp--remove-if-match
+            str (fuzzy-regexp--patterns-one-char-removed str))
+           (fuzzy-regexp--patterns-one-char-inserted str ".")
+           (fuzzy-regexp--patterns-one-char-replaced
             str (lambda (s) (concat "[^" (regexp-quote s) "]")))
-           (%fre-remove-if-match str (%fre-patterns-one-char-transposed str))))
+           (fuzzy-regexp--remove-if-match
+            str (fuzzy-regexp--patterns-one-char-transposed str))))
          (patterns-concise (delete-dups (remq nil (remove "" patterns)))))
     patterns-concise))
 
-(defun %fre-one-char-modified (str &optional word-match self-inclusive)
+(defun fuzzy-regexp--one-char-modified (str &optional word-match self-inclusive)
   "A naive substitute for regexp-opt that keeps regexp special
 characters as-is (not quoted) internally.
 Returns regexp.
 If WORD-MATCH is true, regexp tries to match whole words only (not substrings).
 If SELF-INCLUSIVE is true, regexp also matches STR itself."
-  (let* ((pats-src (%fre-patterns-one-char-modified str))
+  (let* ((pats-src (fuzzy-regexp--patterns-one-char-modified str))
          (pats-fin (if self-inclusive (cons str pats-src) pats-src))
          (re-src (mapconcat 'identity pats-fin "\\|"))
          (re-fin (if word-match (apply 'concat `("\\b\\(?:" ,re-src "\\)\\b")) re-src)))
@@ -119,6 +122,6 @@ If SELF-INCLUSIVE is true, regexp also matches STR itself."
 character.
 If WORD-MATCH is true, regexp tries to match whole words only (not substrings).
 If INCLUDE-SELF is true, regexp alsos matches STR itself."
-  (%fre-one-char-modified str word-match include-self))
+  (fuzzy-regexp--one-char-modified str word-match include-self))
 
 (provide 'fuzzy-regexp)
--- test/test-fuzzy-regexp.el
+++ test/test-fuzzy-regexp.el
@@ -7,91 +7,91 @@
 (require 'ert)
 (require 'fuzzy-regexp)
 
-(ert-deftest test-%fre-replace-substring ()
+(ert-deftest test-fuzzy-regexp--replace-substring ()
   (should (equal
            '("aXcdef"
              "abXf"
              "abCDef"
              "ab<C>def")
-           `(,(%fre-replace-substring "abcdef" "X" 1 2)
-             ,(%fre-replace-substring "abcdef" "X" 2 5)
-             ,(%fre-replace-substring "abcdef" (lambda (s) (upcase s)) 2 4)
-             ,(%fre-replace-substring "abcdef"
+           `(,(fuzzy-regexp--replace-substring "abcdef" "X" 1 2)
+             ,(fuzzy-regexp--replace-substring "abcdef" "X" 2 5)
+             ,(fuzzy-regexp--replace-substring "abcdef" (lambda (s) (upcase s)) 2 4)
+             ,(fuzzy-regexp--replace-substring "abcdef"
                                   (lambda (s) (concat "<" (upcase s) ">"))
                                   2 3)))))
 
-(ert-deftest test-%fre-remove-substring ()
+(ert-deftest test-fuzzy-regexp--remove-substring ()
   (should (equal
            '("ac"
              "abf")
-           `(,(%fre-remove-substring "abc" 1 2)
-             ,(%fre-remove-substring "abcdef" 2 5)))))
+           `(,(fuzzy-regexp--remove-substring "abc" 1 2)
+             ,(fuzzy-regexp--remove-substring "abcdef" 2 5)))))
 
-(ert-deftest test-%fre-insert-substring ()
+(ert-deftest test-fuzzy-regexp--insert-substring ()
   (should (equal
            '("XYabc"
              "aXYbc")
-           `(,(%fre-insert-substring "abc" 0 "XY")
-             ,(%fre-insert-substring "abc" 1 "XY")))))
+           `(,(fuzzy-regexp--insert-substring "abc" 0 "XY")
+             ,(fuzzy-regexp--insert-substring "abc" 1 "XY")))))
 
-(ert-deftest test-%fre-transpose-substring ()
+(ert-deftest test-fuzzy-regexp--transpose-substring ()
   (should (equal
            '("ba"
              "bac"
              "acb")
-           `(,(%fre-transpose-substring "ab" 0)
-             ,(%fre-transpose-substring "abc" 0)
-             ,(%fre-transpose-substring "abc" 1)))))
+           `(,(fuzzy-regexp--transpose-substring "ab" 0)
+             ,(fuzzy-regexp--transpose-substring "abc" 0)
+             ,(fuzzy-regexp--transpose-substring "abc" 1)))))
 
-(ert-deftest test-%fre-patterns-one-char-removed ()
+(ert-deftest test-fuzzy-regexp--patterns-one-char-removed ()
   (should (equal
            '(("")
              ("")
              ("[^a]b" "a[^b]")
              ("[^a]bc" "ac" "ab[^c]")
              ("[^a]bcd" "acd" "abd" "abc[^d]"))
-           `(,(%fre-patterns-one-char-removed "")
-             ,(%fre-patterns-one-char-removed "a")
-             ,(%fre-patterns-one-char-removed "ab")
-             ,(%fre-patterns-one-char-removed "abc")
-             ,(%fre-patterns-one-char-removed "abcd")))))
+           `(,(fuzzy-regexp--patterns-one-char-removed "")
+             ,(fuzzy-regexp--patterns-one-char-removed "a")
+             ,(fuzzy-regexp--patterns-one-char-removed "ab")
+             ,(fuzzy-regexp--patterns-one-char-removed "abc")
+             ,(fuzzy-regexp--patterns-one-char-removed "abcd")))))
 
-(ert-deftest test-%fre-patterns-one-char-inserted ()
+(ert-deftest test-fuzzy-regexp--patterns-one-char-inserted ()
   (should (equal
            '(("")
              ("")
              ("axb")
              ("axbc" "abxc"))
-           `(,(%fre-patterns-one-char-inserted "" "x")
-             ,(%fre-patterns-one-char-inserted "a" "x")
-             ,(%fre-patterns-one-char-inserted "ab" "x")
-             ,(%fre-patterns-one-char-inserted "abc" "x")))))
+           `(,(fuzzy-regexp--patterns-one-char-inserted "" "x")
+             ,(fuzzy-regexp--patterns-one-char-inserted "a" "x")
+             ,(fuzzy-regexp--patterns-one-char-inserted "ab" "x")
+             ,(fuzzy-regexp--patterns-one-char-inserted "abc" "x")))))
 
-(ert-deftest test-%fre-patterns-one-char-replaced ()
+(ert-deftest test-fuzzy-regexp--patterns-one-char-replaced ()
   (should (equal
            '(("")
              ("x")
              ("xb" "ax")
              ("xbc" "axc" "abx"))
-           `(,(%fre-patterns-one-char-replaced "" "x")
-             ,(%fre-patterns-one-char-replaced "a" "x")
-             ,(%fre-patterns-one-char-replaced "ab" "x")
-             ,(%fre-patterns-one-char-replaced "abc" "x")))))
+           `(,(fuzzy-regexp--patterns-one-char-replaced "" "x")
+             ,(fuzzy-regexp--patterns-one-char-replaced "a" "x")
+             ,(fuzzy-regexp--patterns-one-char-replaced "ab" "x")
+             ,(fuzzy-regexp--patterns-one-char-replaced "abc" "x")))))
 
-(ert-deftest test-%fre-patterns-one-char-transposed ()
+(ert-deftest test-fuzzy-regexp--patterns-one-char-transposed ()
   (should (equal
            '(("")
              ("a")
              ("ba")
              ("bac" "acb")
              ("bacd" "acbd" "abdc"))
-           `(,(%fre-patterns-one-char-transposed "")
-             ,(%fre-patterns-one-char-transposed "a")
-             ,(%fre-patterns-one-char-transposed "ab")
-             ,(%fre-patterns-one-char-transposed "abc")
-             ,(%fre-patterns-one-char-transposed "abcd")))))
+           `(,(fuzzy-regexp--patterns-one-char-transposed "")
+             ,(fuzzy-regexp--patterns-one-char-transposed "a")
+             ,(fuzzy-regexp--patterns-one-char-transposed "ab")
+             ,(fuzzy-regexp--patterns-one-char-transposed "abc")
+             ,(fuzzy-regexp--patterns-one-char-transposed "abcd")))))
 
-(ert-deftest test-%fre-patterns-one-char-modified ()
+(ert-deftest test-fuzzy-regexp--patterns-one-char-modified ()
   (should (equal
            '(()
              ("[^a]")
@@ -101,14 +101,14 @@
              ("[^a]a" "a[^a]" "a.a")
              ("[^a]bb" "ab[^b]"
               "a.bb" "ab.b" "a[^b]b" "bab"))
-           `(,(%fre-patterns-one-char-modified "")
-             ,(%fre-patterns-one-char-modified "a")
-             ,(%fre-patterns-one-char-modified "ab")
-             ,(%fre-patterns-one-char-modified "abc")
-             ,(%fre-patterns-one-char-modified "aa")
-             ,(%fre-patterns-one-char-modified "abb")))))
+           `(,(fuzzy-regexp--patterns-one-char-modified "")
+             ,(fuzzy-regexp--patterns-one-char-modified "a")
+             ,(fuzzy-regexp--patterns-one-char-modified "ab")
+             ,(fuzzy-regexp--patterns-one-char-modified "abc")
+             ,(fuzzy-regexp--patterns-one-char-modified "aa")
+             ,(fuzzy-regexp--patterns-one-char-modified "abb")))))
 
-(ert-deftest test-%fre-one-char-modified ()
+(ert-deftest test-fuzzy-regexp--one-char-modified ()
   (should (equal
            '(""
              "[^a]"
@@ -116,26 +116,26 @@
              "[^a]bc\\|ac\\|ab[^c]\\|a.bc\\|ab.c\\|a[^b]c\\|bac\\|acb"
              "[^a]a\\|a[^a]\\|a.a"
              "[^a]bb\\|ab[^b]\\|a.bb\\|ab.b\\|a[^b]b\\|bab")
-           `(,(%fre-one-char-modified "")
-             ,(%fre-one-char-modified "a")
-             ,(%fre-one-char-modified "ab")
-             ,(%fre-one-char-modified "abc")
-             ,(%fre-one-char-modified "aa")
-             ,(%fre-one-char-modified "abb")))))
+           `(,(fuzzy-regexp--one-char-modified "")
+             ,(fuzzy-regexp--one-char-modified "a")
+             ,(fuzzy-regexp--one-char-modified "ab")
+             ,(fuzzy-regexp--one-char-modified "abc")
+             ,(fuzzy-regexp--one-char-modified "aa")
+             ,(fuzzy-regexp--one-char-modified "abb")))))
 
-(ert-deftest test-%fre-one-char-modified-word-match ()
+(ert-deftest test-fuzzy-regexp--one-char-modified-word-match ()
   (should (equal
            '("[^a]bc\\|ac\\|ab[^c]\\|a.bc\\|ab.c\\|a[^b]c\\|bac\\|acb"
              "\\b\\(?:[^a]bc\\|ac\\|ab[^c]\\|a.bc\\|ab.c\\|a[^b]c\\|bac\\|acb\\)\\b")
-           `(,(%fre-one-char-modified "abc" nil)
-             ,(%fre-one-char-modified "abc" t)))))
+           `(,(fuzzy-regexp--one-char-modified "abc" nil)
+             ,(fuzzy-regexp--one-char-modified "abc" t)))))
 
-(ert-deftest test-%fre-one-char-modified-self-inclusive ()
+(ert-deftest test-fuzzy-regexp--one-char-modified-self-inclusive ()
   (should (equal
            '("[^a]bc\\|ac\\|ab[^c]\\|a.bc\\|ab.c\\|a[^b]c\\|bac\\|acb"
              "abc\\|[^a]bc\\|ac\\|ab[^c]\\|a.bc\\|ab.c\\|a[^b]c\\|bac\\|acb")
-           `(,(%fre-one-char-modified "abc" nil nil)
-             ,(%fre-one-char-modified "abc" nil t)))))
+           `(,(fuzzy-regexp--one-char-modified "abc" nil nil)
+             ,(fuzzy-regexp--one-char-modified "abc" nil t)))))
 
 (ert-deftest test-fuzzy-regexp ()
   (should (equal
